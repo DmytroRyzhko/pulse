76cc7a801acc8a390f2cc15c669678eb97f22082
Dmitriy Ryzhko <ryzhko.dmitry@gmail.com>
2021-01-26 20:49:19 +0200

    Added media queries

--- src/js/script.js
+++ src/js/script.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
     prevArrow: '<button type="button" class="slick-prev"><img src="img/carousel/chevron-left-solid.png" alt="prev"></button>',
     nextArrow: '<button type="button" class="slick-next"><img src="img/carousel/chevron-right-solid.png" alt="next"></button>',
     responsive: [{
-      breakpoint: 992,
+      breakpoint: 1024,
       settings: {
         arrows: false,
         dots: true
